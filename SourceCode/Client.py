018ad0ceb00c2cec9be7ade586b75f0856c7ef4e
Dominicus Dylan Haryoto <dylanharyoto@gmail.com>
2025-03-20 22:30:55 +0800

    put client and server file under source code folder

---
+++ SourceCode/Client.py
@@ -0,0 +1,73 @@
+import sqlite3
+import re
+import bcrypt
+
+DATABASE_FILE = "comp3334.db"
+def initDatabase():
+    conn = sqlite3.connect(DATABASE_FILE);
+    cursor = conn.cursor();
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password TEXT NOT NULL,
+            key TEXT NOT NULL
+            )
+    ''')
+    conn.commit()
+    conn.close()
+
+def registerUser():
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    while True:
+        username = input("Enter a unique username: ").strip()
+        if username == "q":
+            conn.close()
+            return 
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username, ))
+        if cursor.fetchone():
+            print("[ERROR] Username already exists. Please choose a different one, or type \"q\" to exit. ")
+        else:
+            break
+    while True:
+        password1 = input("Enter a password (at least 8 characters, containing only A-Z, a-z, 0-9, @#$%^&+=): ").strip()
+        if not re.fullmatch(r'.{8,}', password1):
+            print("[ERROR] Password must be at least 8 characters long.")
+        else:
+            break
+    while True:
+        password2 = input("Confirm your password: ").strip()
+        if password1 != password2:
+            print("[ERROR] Passwords do not match. Please try again.")
+        else:
+            break
+    hashedPassword = bcrypt.hashpw(password1.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashedPassword))
+    conn.commit()
+    print("[STATUS] User registered successfully!")
+    conn.close()
+
+def userManagement():
+    while True:
+        print("\nUser Management Menu:")
+        print("1. Register User")
+        print("2. Log In")
+        print("3. Reset Password")
+        print("4. Exit")
+        choice = input("Enter your choice: ").strip()
+        if choice == "1":
+            registerUser()
+        elif choice == "2":
+            # login_user()
+            return
+        elif choice == "3":
+            # reset_password()
+            return
+        elif choice == "4":
+            print("Exiting User Management.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    userManagement()
