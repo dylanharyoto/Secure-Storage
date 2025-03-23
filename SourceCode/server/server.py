0569a02e78331ff3dd8b6578a87c9d287c7101ec
Dominicus Dylan Haryoto <dylanharyoto@gmail.com>
2025-03-23 20:08:09 +0800

    implement server

--- SourceCode/server/server.py
+++ SourceCode/server/server.py
@@ -1,12 +1,35 @@
 import sqlite3
 from SourceCode.shared.utils import generate_aes, hash_password, split_aes
 
-def init_conn(db_filename):
-    conn = sqlite3.connect(db_filename)
+# Global Variables
+conn, cursor = None, None
+
+# Constants
+DB_FILENAME="users.db"
+
+def open_conn():
+    conn = sqlite3.connect(DB_FILENAME)
     cursor = conn.cursor()
-    return (conn, cursor)
+
+def close_conn():
+    conn.close()
+
+def check_username_exists(username):
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    if cursor.fetchone():
+        return True
+    return False
 
 def register_user(username, password):
-    hashed_password = hash_password(password)
     client_aes, server_aes = split_aes(generate_aes())
+    cursor.execute("INSERT INTO users (username, password, key) VALUES (?, ?, ?)", (username, hash_password(password), server_aes))
+    conn.commit()
+    return client_aes
 
+def login_user(username, password):
+    cursor.execute("SELECT password FROM users WHERE username = ?", (username, ))
+    return cursor.fetchone() == hash_password(password)
+
+def reset_password(username, password):
+    cursor.execute("UPDATE users SET password = ? WHERE username = ?", (hash_password(password), username))
+    conn.commit()
