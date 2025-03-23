2428a53151cceb5d30297cb54a9f1a5ecb4a2b20
Dominicus Dylan Haryoto <dylanharyoto@gmail.com>
2025-03-23 21:53:34 +0800

    finish login_user_IO

--- SourceCode/client/user_management.py
+++ SourceCode/client/user_management.py
@@ -11,7 +11,7 @@ class UserManagement:
             username, password1, password2 = None, None, None
             while not (flag_username and flag_password1 and flag_password2):
                 if not flag_username:
-                    username = input('Enter your email address (or type "q" for EXIT):\n> ').strip()
+                    username = input('Enter your email address (or type "q" to EXIT):\n> ').strip()
                     if username == "q":
                         return None
                     if not check_username_regex(username):
@@ -22,7 +22,7 @@ class UserManagement:
                         continue
                     flag_username = True
                 elif not flag_password1:
-                    password1 = input('Enter a password with at least 8 characters (or type "q" for EXIT, "b" for BACK):\n> ').strip()
+                    password1 = input('Enter a password with at least 8 characters (or type "q" to EXIT, "b" to BACK):\n> ').strip()
                     if password1 == "q":
                         return None
                     if password1 == "b":
@@ -33,7 +33,7 @@ class UserManagement:
                         continue
                     flag_password1 = True
                 elif not flag_password2:
-                    password2 = input('Confirm your password (or type "q" for EXIT, "b" for BACK):\n> ').strip()
+                    password2 = input('Confirm your password (or type "q" to EXIT, "b" to BACK):\n> ').strip()
                     if password2 == "q":
                         return None
                     if password2 == "b":
@@ -49,40 +49,42 @@ class UserManagement:
         finally:
             self.server.close_conn()
 
-    def login_user_IO():
-        server.open_conn()
-        flag_username, flag_password = False, False
-        while not (flag_username and flag_password):
-            if not flag_username:
-                username = input('Enter your username address (or type "q" for EXIT):\n> ').strip()
-                if username == "q":
-                    server.close_conn()
-                    return
-                if not check_username_regex(username):
-                    print('[ERROR] Invalid email format. Please enter a valid username address.')
-                    continue
-                if not server.check_username_exists(username):
-                    print("[ERROR] Email not found. Please try again.")
-                    continue
-                flag_username = True
-            elif not flag_password:
-                password = input('Enter your password (or type "q" for EXIT, "b" for BACK):\n> ').strip()
-                if password == "q":
-                    server.close_conn()
-                    return
-                if password == "b":
-                    flag_username = False
-                    break
-                if not check_password_regex(password):
-                    print("[ERROR] Password must be at least 8 characters long!")
-                    continue
-                if not server.login_user(username, password):
-                    print("[ERROR] Incorrect password. Please try again.")
-                    continue
-                flag_password = True
-        print(f"[STATUS] Login successful! Welcome, {username}.")
-        server.close_conn()
-        return True
+    def login_user_IO(self):
+        self.server.open_conn()
+        try:
+            flag_username, flag_password = False, False
+            username = None
+            while not (flag_username and flag_password):
+                if not flag_username:
+                    username = input('Enter your email address (or type "q" to EXIT):\n> ').strip()
+                    if username == "q":
+                        return False
+                    if not check_username_regex(username):
+                        print('[ERROR] Invalid email format. Please enter a valid email address.')
+                        continue
+                    if not self.server.check_username_exists(username):
+                        print("[ERROR] Email not found. Please try again.")
+                        continue
+                    flag_username = True
+                elif not flag_password:
+                    password = input('Enter your password (or type "q" to EXIT, "b" to BACK):\n> ').strip()
+                    if password == "q":
+                        return False
+                    if password == "b":
+                        flag_username = False
+                        break
+                    if not check_password_regex(password):
+                        print("[ERROR] Password must be at least 8 characters long!")
+                        continue
+                    if not self.server.login_user(username, password):
+                        print("[ERROR] Incorrect password. Please try again.")
+                        continue
+                    flag_password = True
+            print(f"[STATUS] Login successful! Welcome, {username}.")
+            return True
+        finally:
+            self.server.close_conn()
+
 
     def reset_password_IO():
         server.open_conn()
